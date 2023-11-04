daa2eec071dd6ef96b6b1db7edd6dc152772bbdd
Yashaswe <amatyayasu@gmail.com>
2023-11-04 14:44:53 -0400

    add test data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,8 +18,9 @@ const { Meta } = Card;
 function App() {
   let postinfo = {
     title: "Task",
-
     description:
+      "dlghsldgjslIf you're looking for randomIf you're looIf you're looking for random paragraphs, you've come to the right place. When a random word or a random senking for random paragraphs, you've come to the right place. When a random word or a random sen paragraphs, you've come to the right place. When a random word or a random sen",
+    description2:
       "If you're looking for random paragraphs, you've come to the right place. When a random word or a random sentence isn't quite enough, the next logical step is to find a random paragraph. We created the Random Paragraph Generator with you in mind. The process is quite simple. Choose the number of random paragraphs you'd like to see and click the button. Your chosen number of paragraphs will instantly appear.If you're looking for random paragraphs, you've come to the right place. When a random word or a random sentence isn't quite enough, the next logical step is to find a random paragraph. We created the Random Paragraph Generator with you in mind. The process is quite simple. Choose the number of random paragraphs you'd like to see and click the button. Your chosen number of paragraphs will instantly appear.",
     location: "Bullet Hole",
   };

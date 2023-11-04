73b43a0adfc0255ba43946199277fc02c1f91573
Sanij Shrestha <saniz.sth123@gmail.com>
2023-11-04 14:26:05 -0400

    routing added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,96 +1,27 @@
 import "./App.css";
 import React, { useState } from "react";
-import {
-  UploadOutlined,
-  UserOutlined,
-  MessageOutlined,
-  VideoCameraOutlined,
-  SettingOutlined,
-  EditOutlined,
-  EllipsisOutlined,
-} from "@ant-design/icons";
-import { Avatar, Menu, Button, Layout, Card, Tag, FloatButton } from "antd";
+import { Link } from "react-router-dom";
+import Main from "./pages/Main";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CardPost from "./components/CardPost";
-import { PlusOutlined } from '@ant-design/icons';
+import Card from "antd/es/card/Card";
+import NewHelp from "./pages/NewHelp";
+import Blogs from "./pages/Blogs";
 
-const { Header, Content, Footer, Sider } = Layout;
+
 const { Meta } = Card;
 
 function App() {
-  let postinfo = {
-    title: "Task",
-
-    description:
-      "If you're looking for random paragraphs, you've come to the right place. When a random word or a random sentence isn't quite enough, the next logical step is to find a random paragraph. We created the Random Paragraph Generator with you in mind. The process is quite simple. Choose the number of random paragraphs you'd like to see and click the button. Your chosen number of paragraphs will instantly appear.If you're looking for random paragraphs, you've come to the right place. When a random word or a random sentence isn't quite enough, the next logical step is to find a random paragraph. We created the Random Paragraph Generator with you in mind. The process is quite simple. Choose the number of random paragraphs you'd like to see and click the button. Your chosen number of paragraphs will instantly appear.",
-    location: "Bullet Hole",
-  };
   return (
-    <Layout>
-      <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={(broken) => {
-          console.log(broken);
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-        style={{
-          overflow: "auto",
-          height: "100vh",
-          position: "fixed",
-          left: 0,
-          top: 0,
-          bottom: 0,
-        }}
-      >
-        <div className="demo-logo-vertical" />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={["1"]}
-          items={[
-            { name: "Emergency", icon: <UserOutlined /> },
-            { name: "Find Task" },
-            { name: "Profile", icon: <UserOutlined /> },
-            { name: "", icon: <MessageOutlined /> },
-          ].map((nav, index) => ({
-            key: String(index + 1),
-            icon: nav.icon,
-            label: nav.name,
-          }))}
-        />
-      </Sider>
-      <Layout style={{ marginLeft: 200 }}>
-        <Content
-          style={{
-            margin: "24px 16px 0",
-            top: 0,
-            bottom: 0,
-            // position: "fixed",
-          }}
-        >
-          <div
-            style={{
-              padding: 24,
-              minHeight: 360,
-              background: "white",
-            }}
-          >
-            <CardPost postinfo={postinfo} />
-            <CardPost postinfo={postinfo} />
-            <CardPost postinfo={postinfo} />
-            <CardPost postinfo={postinfo} />
-          </div>
-          {/* <FloatButton onClick={() => console.log("click")} /> */}
-          <FloatButton className="addPost" icon={<PlusOutlined />} type="primary" style={{ right: 100, width: 80, height: 80}} tooltip={<div>New Help</div>} />
-        </Content>
-        <Footer style={{ textAlign: "center" }}>
-          Yashaswe, Sanij, Truc, Prabesh
-        </Footer>
-      </Layout>
-    </Layout>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+          <Route path="/newhelp" element={<NewHelp />} />
+          <Route path="/blogs" element = {<Blogs />} />
+          
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;

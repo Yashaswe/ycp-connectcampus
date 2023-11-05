fe0156d8faabfc970064c5195ed3a8cccd3ac030
Sanij Shrestha <saniz.sth123@gmail.com>
2023-11-05 01:02:49 -0400

    fixed inbox error

--- frontend/src/pages/Inbox.js
+++ frontend/src/pages/Inbox.js
@@ -15,61 +15,97 @@ import { redirect } from 'react-router-dom';
 import Profile from './Profile'
 
 
-const Inbox = () => {
-   
-    const [filters, setFilters] = useState(null)
-    const [options, setOptions] = useState(null)
-    const [sort, setSort] = useState(null)
-    const [chatClient, setChatClient] = useState(null)
+const apiKey = "48kmaj4gqgva";
 
-    useEffect(async() => {
+const user = {
+  id: 'thuc',
+  name: "thuc"
+}
+const userId = user.id; 
 
-        const apiKey = "48kmaj4gqgva";
+const chatClient = StreamChat.getInstance(apiKey);
 
-        if (!localStorage.getItem("email") || !localStorage.getItem("name")) {
-            return redirect('/login')
-        }
-        
-        const user = {
-          id: localStorage.getItem("email").replace(/[^a-z0-9@]/g, ''),
-          name: localStorage.getItem("name")
-        }
-        const userId = user.id; 
-        
-        setChatClient(StreamChat.getInstance(apiKey));
-        
-        if (typeof window !== 'undefined') {
-          chatClient.connectUser({ id: userId }, chatClient.devToken(userId));
-        }
-        const channel = chatClient.channel('messaging', 'gfc', {
-          name: 'Awesome channel about traveling',
-          members: [userId]
-        });
-        // Here, 'travel' will be the channel ID
-        await channel.watch();
-        // if (process.env.REACT_APP_CHAT_SERVER_ENDPOINT) {
-        //   chatClient.setBaseURL(process.env.REACT_APP_CHAT_SERVER_ENDPOINT);
-        // }
-        setFilters({ type: 'messaging', members: {$in: [userId]} });
-        setOptions({ state: true, presence: true, limit: 10 }) 
-        setSort({ last_message_at: -1, updated_at: -1 });
-    }, []);
+if (typeof window !== 'undefined') {
+  chatClient.connectUser({ id: userId }, chatClient.devToken(userId));
+}
+const channel = chatClient.channel('messaging', 'travel', {
+  name: 'Awesome channel about traveling',
+  members: [userId]
+});
+// Here, 'travel' will be the channel ID
+await channel.watch();
+// if (process.env.REACT_APP_CHAT_SERVER_ENDPOINT) {
+//   chatClient.setBaseURL(process.env.REACT_APP_CHAT_SERVER_ENDPOINT);
+// }
+const filters = { type: 'messaging', members: {$in: [userId]}  };
+const options = { state: true, presence: true, limit: 10 };
+const sort = { last_message_at: -1, updated_at: -1 };
 
-    console.log(localStorage.getItem("name"));
-    return (
-        <Chat client={chatClient}>
-            <ChannelList filters={filters} options={options} showChannelSearch sort={sort} />
-            <Channel>
-            <Window>
-                <ChannelHeader />
-                <MessageList />
-                <MessageInput focus />
-            </Window>
-            <Thread />
-            </Channel>
-        </Chat>
-    )
-  
-};
+const Inbox = () => (
+  <Chat client={chatClient}>
+    <ChannelList filters={filters} options={options} showChannelSearch sort={sort} />
+    <Channel>
+      <Window>
+        <ChannelHeader />
+        <MessageList />
+        <MessageInput focus />
+      </Window>
+      <Thread />
+    </Channel>
+  </Chat>
+);
 
 export default Inbox;
+//     useEffect(async() => {
+
+//         const apiKey = "48kmaj4gqgva";
+
+//         if (!localStorage.getItem("email") || !localStorage.getItem("name")) {
+//             console.log(localStorage.getItem("name"));
+//             return redirect('/login')
+//         }
+//         setUser({
+//             id: localStorage.getItem("email").replace(/[^a-z0-9@]/g, ''),
+//             name: localStorage.getItem("name")
+//           })
+//           console.log(localStorage.getItem("name"));
+        
+//         const userId = user.id; 
+        
+//         setChatClient(StreamChat.getInstance(apiKey));
+        
+//         if (typeof window !== 'undefined') {
+//           chatClient.connectUser({ id: userId }, chatClient.devToken(userId));
+//         }
+//         const channel = chatClient.channel('messaging', 'gfc', {
+//           name: 'Awesome channel about traveling',
+//           members: [userId]
+//         });
+//         // Here, 'travel' will be the channel ID
+//         await channel.watch();
+//         // if (process.env.REACT_APP_CHAT_SERVER_ENDPOINT) {
+//         //   chatClient.setBaseURL(process.env.REACT_APP_CHAT_SERVER_ENDPOINT);
+//         // }
+//         setFilters({ type: 'messaging', members: {$in: [userId]} });
+//         setOptions({ state: true, presence: true, limit: 10 }) 
+//         setSort({ last_message_at: -1, updated_at: -1 });
+//     }, []);
+
+//     console.log(localStorage.getItem("name"));
+//     return (
+//         <Chat client={chatClient}>
+//             <ChannelList filters={filters} options={options} showChannelSearch sort={sort} />
+//             <Channel>
+//             <Window>
+//                 <ChannelHeader />
+//                 <MessageList />
+//                 <MessageInput focus />
+//             </Window>
+//             <Thread />
+//             </Channel>
+//         </Chat>
+//     )
+  
+// };
+
+// export default Inbox;

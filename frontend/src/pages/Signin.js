24b110178f90bfc6d7690f7d0246d50953377871
Yashaswe <amatyayasu@gmail.com>
2023-11-04 21:11:34 -0400

    authenticate user and navigate accordingly

--- frontend/src/pages/Signin.js
+++ frontend/src/pages/Signin.js
@@ -1,68 +1,87 @@
+import React from "react";
+import { Link } from "react-router-dom";
 
-import React from 'react';
-import {Link} from "react-router-dom"
-
-import { Form, Input, Button, Checkbox } from 'antd';
-import { MailOutlined, LockOutlined } from '@ant-design/icons';
+import { Form, Input, Button, Checkbox } from "antd";
+import { MailOutlined, LockOutlined } from "@ant-design/icons";
 
 export default function Signin() {
-  const onFinish = values => {
-    console.log('Received values of form: ', values);
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
   };
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-    <Form
-      name="normal_login"
-      className="login-form"
-      initialValues={{
-        remember: true,
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
       }}
-      onFinish={onFinish}
     >
-      <Form.Item
-        name="email"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your email!',
-          },
-        ]}
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
       >
-        <Input prefix={<MailOutlined />} placeholder="Email" />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your Password!',
-          },
-        ]}
-      >
-        <Input
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          type="password"
-          placeholder="Password"
-        />
-      </Form.Item>
-      <Form.Item>
-        <Form.Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox>Remember me</Checkbox>
+        <Form.Item
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Please input your email!",
+            },
+          ]}
+        >
+          <Input prefix={<MailOutlined />} placeholder="Email" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Password!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
         </Form.Item>
 
-
-      </Form.Item>
-
-      <Form.Item>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                <Button type="primary" htmlType="submit" className="signup-form-button">
-                Log in!
-                </Button>
-                <span>Don't have an account?<Link to = "./signup"><a href="">Sign up!</a></Link></span>
+        <Form.Item>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="signup-form-button"
+            >
+              Log in!
+            </Button>
+            <span>
+              Don't have an account?
+              <Link to="/signup">
+                <a href="">Sign up!</a>
+              </Link>
+            </span>
           </div>
-      </Form.Item>
-    </Form>
+        </Form.Item>
+      </Form>
     </div>
   );
-};
+}
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,141 +1,171 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Form, Input, Button, Upload } from 'antd';
-import { UserOutlined, LockOutlined, MailOutlined, UploadOutlined } from '@ant-design/icons';
-import axios from 'axios'
-
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Form, Input, Button, Upload } from "antd";
+import {
+  UserOutlined,
+  LockOutlined,
+  MailOutlined,
+  UploadOutlined,
+} from "@ant-design/icons";
+import axios from "axios";
 
 export default function Signup() {
+  const navigate = useNavigate();
+  const onFinish = async (values) => {
+    try {
+      const response = await axios.post("/authentication/signup", {
+        email: values.email,
+        name: values.name,
+        password: values.password,
+      });
 
-    const onFinish = async(values)=> {
-        try{
-          const response=await axios.post("/authentication/signup",{
-            "email":values.email,
-            "name":values.name,
-            "password":values.password
-          });
+      const token = response.data.token;
+      if (token) {
+        localStorage.setItem("authToken", token);
+        navigate("/");
+      }
 
-          const token = response.data.token
-          //store 
-        
-        } catch(err){
-          console.log(err)
-        }
+      //store
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-  
-      };
-      
-      const normFile = (e) => {
-        console.log('Upload event:', e);
-        if (Array.isArray(e)) {
-          return e;
-        }
-        return e?.fileList;
-      };
-    
-      return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-        <Form
-          name="signup"
-          className="signup-form"
-          initialValues={{
-            remember: true,
-          }}
-          onFinish={onFinish}
-        ><Form.Item
-            name="email"
-            rules={[
-              {
-                type: "email",
-                message: "The input is not valid E-mail!",
-              },
-              {
-                required: true,
-                message: "Please input your College E-mail!",
-              },
-            ]}
-            hasFeedback
-          >
-            <Input prefix={<MailOutlined />} placeholder="Email" />
-          </Form.Item>
-          <Form.Item
-            name="name"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your Name!',
-              },
-            ]}
-          >
-            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Full name" />
-          </Form.Item>
-          <Form.Item
-      name="upload"
-      label="Upload"
-      valuePropName="fileList"
-      getValueFromEvent={normFile}
-      extra="Should be less than 5MB!"
+  const normFile = (e) => {
+    console.log("Upload event:", e);
+    if (Array.isArray(e)) {
+      return e;
+    }
+    return e?.fileList;
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
     >
-      <Upload name="logo" action="/upload.do" listType="picture">
-        <Button icon={<UploadOutlined />}>Click to upload</Button>
-      </Upload>
-    </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: "Please input your password, minimum character is 6!",
-                min: 6,
-                max: 24,
-              },
-            ]}
-            hasFeedback
-          >
-            <Input.Password
-              prefix={<LockOutlined />}
-              type="password"
-              placeholder="Password"
-            />
-          </Form.Item>
+      <Form
+        name="signup"
+        className="signup-form"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="email"
+          rules={[
+            {
+              type: "email",
+              message: "The input is not valid E-mail!",
+            },
+            {
+              required: true,
+              message: "Please input your College E-mail!",
+            },
+          ]}
+          hasFeedback
+        >
+          <Input prefix={<MailOutlined />} placeholder="Email" />
+        </Form.Item>
+        <Form.Item
+          name="name"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Name!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Full name"
+          />
+        </Form.Item>
+        <Form.Item
+          name="upload"
+          label="Upload"
+          valuePropName="fileList"
+          getValueFromEvent={normFile}
+          extra="Should be less than 5MB!"
+        >
+          <Upload name="logo" action="/upload.do" listType="picture">
+            <Button icon={<UploadOutlined />}>Click to upload</Button>
+          </Upload>
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password, minimum character is 6!",
+              min: 6,
+              max: 24,
+            },
+          ]}
+          hasFeedback
+        >
+          <Input.Password
+            prefix={<LockOutlined />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
 
-          <Form.Item
-            name="confirm"
-            dependencies={["password"]}
-            hasFeedback
-            placeholder="Confirm Password"
-            rules={[
-              {
-                required: true,
-                message: "Please confirm your password!",
-              },
-              ({ getFieldValue }) => ({
-                validator(_, value) {
-                  if (!value || getFieldValue("password") === value) {
-                    return Promise.resolve();
-                  }
+        <Form.Item
+          name="confirm"
+          dependencies={["password"]}
+          hasFeedback
+          placeholder="Confirm Password"
+          rules={[
+            {
+              required: true,
+              message: "Please confirm your password!",
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue("password") === value) {
+                  return Promise.resolve();
+                }
 
-                  return Promise.reject(
-                    new Error(
-                      "The two passwords that you entered do not match!"
-                    )
-                  );
-                },
-              }),
-            ]}
+                return Promise.reject(
+                  new Error("The two passwords that you entered do not match!")
+                );
+              },
+            }),
+          ]}
+        >
+          <Input.Password prefix={<LockOutlined />} />
+        </Form.Item>
+
+        <Form.Item>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
           >
-            <Input.Password prefix={<LockOutlined />} />
-          </Form.Item>
-    
-          <Form.Item>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                <Button type="primary" htmlType="submit" className="signup-form-button">
-                Sign up!
-                </Button>
-                <span>Already have an account? <Link to = "./signin"><a href="">Log in</a></Link></span>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="signup-form-button"
+            >
+              Sign up!
+            </Button>
+            <span>
+              Already have an account?{" "}
+              <Link to="/signin">
+                <a href="">Log in</a>
+              </Link>
+            </span>
           </div>
-          </Form.Item>
-        </Form>
-        </div>
-      );
+        </Form.Item>
+      </Form>
+    </div>
+  );
 }

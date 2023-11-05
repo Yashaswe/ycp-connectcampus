365e97b5aa431dbbfc99af141743116756c6c34d
PrabeshPP <prabesh.bista369@gmail.com>
2023-11-05 01:05:42 -0400

    add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import UserPage from "./pages/UserPage";
 import Inbox from "./pages/Inbox";
+import Profile from "./pages/Profile";
 
 const { Meta } = Card;
 const { Header, Content, Footer, Sider } = Layout;
